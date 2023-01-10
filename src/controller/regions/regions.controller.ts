0b4c2043954ba039662e4da4cb48500600a7a7ed
FaQih <110642627+programmerShinobi@users.noreply.github.com>
2023-01-10 22:18:49 +0700

    Update lessons

--- src/controller/regions/regions.controller.ts
+++ src/controller/regions/regions.controller.ts
@@ -1,31 +1,33 @@
-import { Controller, Get, Res, HttpStatus, Response } from '@nestjs/common';
-import { RegionsService } from 'src/service/regions/regions.service';
-// import { RegionsService } from '../../service/regions/regions.service';
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import {  RegionsService } from 'src/service/regions/regions.service';
 
-@Controller()
+@Controller('regions')
 export class RegionsController {
     constructor(private RegionsService: RegionsService) {}
-
-    // @Get('regions')
-    // async findAll(): Promise<any> {
-    //   return await this.RegionsService.findAll();
-    // }
-  
-    @Get('regions')
-    findAll(): Promise<any> {
+    
+    @Get()
+    findAll() {
       return this.RegionsService.findAll();
     }
-      
-    // @Get('regions')
-    // async findAll(@Res() Response): Promise<any> {
-    //   const result = await this.RegionsService.findAll();
-    //   if (result) {
-    //     return Response.status(HttpStatus.OK).send(result);
-    //   }
-    // }
-  
-    @Get('hellosss')
-    async getHello() {
-    return await this.RegionsService.getHello();
-  }
+    
+    @Get(':id')
+    findOne(@Param() params){
+      return  this.RegionsService.findOne(params.id);
+    }
+    
+    @Post()
+    create(@Body() body){
+      return this.RegionsService.create(body);
+    }
+    
+    @Put(':id')
+    update(@Param('id') id: number, @Body() body: any) {
+      return this.RegionsService.update(id, body)
+    }
+    
+    @Delete(':id')
+    remove(@Param('id') id: number, @Body() body: any) {
+      return this.RegionsService.delete(id);
+    }
+
 }
--- src/service/regions/regions.service.ts
+++ src/service/regions/regions.service.ts
@@ -7,22 +7,34 @@ import { Regions } from '../../../entities/Regions';
 export class RegionsService {
     constructor(
         @InjectRepository(Regions) // @ : decorator
-        private RegionRepository: Repository<Regions>,
+        private regionRepository: Repository<Regions>,
     ) {}
 
     async findAll(): Promise<any> { // Promise<any> is collection, not data type, because use async await
-        return await this.RegionRepository.find();
+        return await this.regionRepository.find();
     }
 
-    // async findAll(): Promise<any> {
-    //     return this.regionRepository.find();
-    // }
-
-    getHello(): string {
-        return `alhamdulillah bisa`;
+    async findOne(id): Promise<any> {
+       return await this.regionRepository.findOneBy({regionId:id})
     }
 
-    // findAll(): any { // Promise<any> is collection, not data type, because use async await
-    //     return  this.regionRepository.find();
-    // }
+    async create(items: Regions): Promise<any>  {
+        await this.regionRepository.save({
+            regionId: items.regionId,
+            regionName: items.regionName
+        });
+        return "Data inserted";
+    }
+
+    async update(id: number, data: Regions): Promise<any>{
+        await this.regionRepository.update(
+            { regionId: id },
+            { regionName: data.regionName });
+        return "Data updated";
+    }
+
+    async delete(id: number): Promise<any> {
+        await this.regionRepository.delete({ regionId: id });
+        return "Data deleted";
+    }
 }
