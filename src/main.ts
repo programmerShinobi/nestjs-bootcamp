fd1c65b8e69ce3d8ca246c2d4fd932b596d2da76
FaQih <110642627+programmerShinobi@users.noreply.github.com>
2023-01-31 11:47:49 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { MainModule } from './main.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(MainModule);
-  const port = process.env.PORT||3005;
+  const port = process.env.PORT || 3005;
+  app.enableCors() // CONNECT TO API
   await app.listen(
     port,
     () => console.info(`Server started at http://localhost:${port}`));
